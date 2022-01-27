5f9f6724116e501b53e62200c7134859e320ca27
RnMor777 <ryan.morganti@trojans.dsu.edu>
2022-01-27 14:52:05 -0600

    Final

--- Calculator.cs
+++ Calculator.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 
 namespace MorgantiCalculator { 
     public partial class Calculator : Form { 
+
         // declare variables
         float? leftOperand = null;
         float? rightOperand = null;
